e1e9c1231a4ed75e93fd41f1d67226ec96988dab
Boyd Lever <boydlever@stanfordhacker.com>
2023-09-21 21:24:44 -0700

    Save

--- server.py
+++ server.py
@@ -1,5 +1,13 @@
 from flask_app import app
 from flask_app.controllers import posting_controller, user_controller
+from datetime import datetime
+
+@app.template_filter('format_datetime')
+def format_datetime(value, format='%B %d, %Y %I:%M %p'):
+    """Convert a datetime to a different format."""
+    if isinstance(value, str):
+        value = datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
+    return value.strftime(format)
 
 if __name__=="__main__":
     app.run(debug=True)
